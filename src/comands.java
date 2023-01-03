29ab51c42c6d380e06046449a9f78795f089aad7
Ilutik Andrey <ilutikandrej@gmail.com>
2023-01-03 18:37:28 +0300

    32

--- src/comands.java
+++ src/comands.java
@@ -3,5 +3,7 @@ public class comands {
 //    git add .
 //    git commit -m "First Adding"
 //    git branch testBranch
-
+//      git branch -M master
+//      git remote add origin https://github.com/Andr0id228/test.git
+//        git push -u origin master
 }
